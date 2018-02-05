ce1654308135ab3afcf6980038dfd3ddff04792f
mesbahulalam <33378442+mesbahulalam@users.noreply.github.com>
2018-02-05 11:27:24 +0000

    Update button.js

--- button.js
+++ button.js
@@ -1,22 +1,19 @@
-var buttons = document.getElementsByTagName('button');
-
-Array.prototype.forEach.call(buttons, function (b) {
-    b.addEventListener('click', createRipple);
-});
-
-function createRipple (e) {
-    var circle = document.createElement('div');
-    this.appendChild(circle);
-
-    var d = Math.max(this.clientWidth, this.clientHeight);
-
-    circle.style.width = circle.style.height = d + 'px';
-
-var rect = this.getBoundingClientRect();
-circle.style.left = e.clientX - rect.left -d/2 + 'px';
-circle.style.top = e.clientY - rect.top - d/2 + 'px';
-
-    console.log(this);
-
-    circle.classList.add('ripple');
+/* the main function */
+function createRipple(e) {
+	var circle = document.createElement('div'),
+	d = Math.max(this.clientWidth, this.clientHeight),
+	rect = this.getBoundingClientRect();
+	this.appendChild(circle);
+	circle.style.width = circle.style.height = d + 'px';
+	circle.style.left = e.clientX - rect.left - d / 2 + 'px';
+	circle.style.top = e.clientY - rect.top - d / 2 + 'px';
+	circle.classList.add('ripple-effect');
+    /* delete creted extra nodes after they fulfill their purpose. */
+	setTimeout(function () {circle.parentNode.removeChild(circle); }, '601');
 }
+/* Calling  the main function */
+/* Now, add ripple class to the elements you want ripple effects on. */
+var els = document.getElementsByClassName('ripple');
+Array.prototype.forEach.call(els, function (el) {
+    el.addEventListener('click', createRipple);
+});
